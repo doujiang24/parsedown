5e05e496b8cc4b10e2f6e781746fb0f85b5c426e
doujiang24 <doujiang24@gmail.com>
2014-07-04 07:00:33 -0400

    add js

---
+++ markdown/js/markdown.js
@@ -0,0 +1,62 @@
+/*! markdown top directory */
+
+var Directory = {
+    id: 0,
+    init: function (li) {
+        var level = $(li).attr("_list_level");
+
+        if ( $(li).next().attr("_list_level") <= level) {
+            return false;
+        }
+
+        var id = "_directory_fold_" + Directory.id++;
+        var button = "<a class='fold_button' href='#' id='" + id + "'>收起</a>";
+
+        $(li).append(button);
+
+        $("#" + id).click(function () {
+            var li = $(this).parent("li");
+            Directory.show(li);
+            return false;
+        });
+
+        Directory.show(li);
+    },
+    show: function (li) {
+        var level = $(li).attr("_list_level");
+        var stats = $(li).find("a.fold_button").html();
+
+        var hide = $(li).find("a.fold_button").html() == "展开" ? true : false;
+        $(li).find("a.fold_button").html(hide ? "收起" : "展开");
+
+        var goon = true;
+
+        $(li).nextAll().each(function (_i, l) {
+            var ll = $(l).attr("_list_level");
+
+            if (goon && ll > level) {
+                if (hide) {
+                    $(l).show();
+
+                } else {
+                    $(l).hide();
+                }
+
+            } else {
+                goon = false;
+            }
+        });
+    },
+    fold: function (level) {
+        $("ul#top_directory").find("li").each(function (_k, li) {
+            if (level == parseInt($(li).attr("_list_level"))) {
+                Directory.init(li);
+            }
+        });
+    },
+}
+
+
+$(function () {
+    Directory.fold(2);
+});
